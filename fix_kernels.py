2a4a271b54057789c0851d956f01dfa73dea3ac6
vmoraleso <vmorales.ppb@gmail.com>
2026-04-21 21:39:02 -0500

    update build

--- fix_kernels.py
+++ fix_kernels.py
@@ -2,8 +2,8 @@ import nbformat
 import os
 
 nuevo_kernel = {
-    "name": "geo_env",  # cambia aquí si deseas otro, como "python-geo-env"
-    "display_name": "Python (geo_env)",  # este es el nombre visible
+    "name": "env_global",
+    "display_name": "Python (env_global)",
     "language": "python"
 }
 
@@ -17,4 +17,4 @@ for fname in os.listdir():
         with open(fname, "w", encoding="utf-8") as f:
             nbformat.write(nb, f)
 
-print("✅ Kernels actualizados a 'geo_env'.")
+print("✅ Kernels actualizados a 'env_global'.")
